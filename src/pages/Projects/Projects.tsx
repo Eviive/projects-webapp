a67cd0908f59af61618577d6948b0ead2923ee52
Eviive <albert.vaillon21@gmail.com>
2023-05-01 15:00:34 +0200

    feat: added basic content to the Projects page

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -1,9 +1,32 @@
-import { FC } from "react";
+import { ProjectService } from "api/services";
+import { Loader } from "components/common";
+import { ProjectCard, ProjectForm } from "components/projects";
+import { useCustomQuery } from "hooks/useCustomQuery";
+import { GridLayout } from "layouts";
+import { FC, useState } from "react";
+import { Project } from "types/entities";
 
 import styles from "./projects.module.scss";
 
 export const Projects: FC = () => {
+
+    const query = useCustomQuery([ "projects" ], ProjectService.findAll);
+
+    const [ editProject, setEditProject ] = useState<Project | null>(null);
+
     return (
-        <h1>Projects</h1>
+        <div className={styles.projectsWrapper}>
+            {query.isSuccess
+
+                ? <div className={styles.projectsContent}>
+                    {editProject && <ProjectForm project={editProject} handleClose={() => setEditProject(null)} />}
+                    <GridLayout className={styles.wrapper} size="350px" gap="1.5em">
+                        {[ ...query.data, ...query.data, ...query.data ].map((project, index) => <ProjectCard key={index} project={project} handleEdit={() => setEditProject(project)} />)}
+                    </GridLayout>
+                </div>
+
+                : <Loader />
+            }
+        </div>
     );
 };
