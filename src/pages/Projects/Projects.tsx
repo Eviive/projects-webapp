1e4c1ef522506e2321a08dffa8f3fc7bf1b24115
Eviive <albert.vaillon21@gmail.com>
2023-05-01 18:23:30 +0200

    fix: centered the loaders

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -15,18 +15,20 @@ export const Projects: FC = () => {
     const [ editProject, setEditProject ] = useState<Project | null>(null);
 
     return (
-        <div className={styles.projectsWrapper}>
-            {query.isSuccess
+        <>
+            { query.isSuccess
 
-                ? <div className={styles.projectsContent}>
-                    {editProject && <ProjectForm project={editProject} handleClose={() => setEditProject(null)} />}
-                    <GridLayout className={styles.wrapper} size="350px" gap="1.5em">
-                        {[ ...query.data, ...query.data, ...query.data ].map((project, index) => <ProjectCard key={index} project={project} handleEdit={() => setEditProject(project)} />)}
-                    </GridLayout>
+                ? <div className={styles.projectsWrapper}>
+                    <div className={styles.projectsContent}>
+                        {editProject && <ProjectForm project={editProject} handleClose={() => setEditProject(null)} />}
+                        <GridLayout className={styles.wrapper} size="350px" gap="1.5em">
+                            {[ ...query.data, ...query.data, ...query.data ].map((project, index) => <ProjectCard key={index} project={project} handleEdit={() => setEditProject(project)} />)}
+                        </GridLayout>
+                    </div>
                 </div>
 
                 : <Loader />
             }
-        </div>
+        </>
     );
 };
