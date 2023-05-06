ffe9f1b5c97c53cb48ac4f23709d380ee99e2faf
Eviive <albert.vaillon21@gmail.com>
2023-05-06 17:40:09 +0200

    feat: using the Popup component where it's needed

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,12 +1,13 @@
 import { UserService } from "api/services";
-import { AxiosError } from "axios";
-import { Button, Input } from "components/common";
+import { Button, Input, Popup } from "components/common";
 import { useAuthContext } from "contexts/AuthContext";
-import { FC, FormEventHandler, useLayoutEffect, useState } from "react";
+import { usePopup } from "hooks/usePopup";
+import { FC, useLayoutEffect, useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { FaLock, FaUser } from "react-icons/fa";
 import { Navigate } from "react-router-dom";
 import { AuthRequest } from "types/forms";
+import { getTitleAndMessage } from "utils/errors";
 
 import styles from "./login.module.scss";
 
@@ -18,6 +19,8 @@ export const Login: FC = () => {
 
     const [ redirect, setRedirect ] = useState(false);
 
+    const [ popup, showPopup ] = usePopup();
+
     const { register, handleSubmit } = useForm<AuthRequest>();
 
     useLayoutEffect(() => {
@@ -26,47 +29,22 @@ export const Login: FC = () => {
         }
     }, [accessToken]);
 
-    const handleInvalid: FormEventHandler<HTMLInputElement> = e => {
-        e.preventDefault(); // prevents the default popup from showing
-        console.log("Invalid input");
-        // showPopup({
-        //     title: "Invalid input",
-        //     message: "Please fill in all the fields."
-        // });
-    };
-
     const submitHandler: SubmitHandler<AuthRequest> = async data => {
-        if (isSubmitting) {
-            return;
-        }
+        if (isSubmitting) return;
         setIsSubmitting(true);
         try {
             const res = await UserService.login(data);
             if (res.roles.includes("ROLE_ADMIN")) {
                 setAccessToken(res.accessToken);
             } else {
-                console.log("Insufficient permissions");
-                // showPopup({
-                //     title: "Insufficient permissions",
-                //     message: "You must be an administrator to access the dashboard."
-                // });
-                setIsSubmitting(false);
+                showPopup({
+                    title: "Insufficient permissions",
+                    message: "You must be an administrator to access the dashboard."
+                });
             }
         } catch (e) {
-            console.log(e);
-            if (e instanceof AxiosError) {
-                console.log(e.response?.data);
-                // showPopup({
-                //     title: e.response?.data?.error ?? e.name,
-                //     message: e.response?.data?.message ?? e.message
-                // });
-            } else {
-                console.log("Unknown error", e);
-                // showPopup({
-                //     title: "Unknown error",
-                //     message: "Please try again later."
-                // });
-            }
+            showPopup(getTitleAndMessage(e));
+        } finally {
             setIsSubmitting(false);
         }
     };
@@ -75,33 +53,34 @@ export const Login: FC = () => {
         <>
             { redirect
                 ? <Navigate to="/" />
-                : <div className={styles.loginWrapper}>
-                    <form
-                        className={styles.loginForm}
-                        onSubmit={handleSubmit(submitHandler)}
-                    >
-                        <Input
-                            attributes={{
-                                ...register("username", { required: true }),
-                                className: styles.inputWrapper,
-                                autoComplete: "username"
-                            }}
-                            label={<FaUser size={14} />}
-                            handleInvalid={handleInvalid}
-                        />
-                        <Input
-                            attributes={{
-                                ...register("password", { required: true }),
-                                className: styles.inputWrapper,
-                                type: "password",
-                                autoComplete: "current-password"
-                            }}
-                            label={<FaLock size={14} />}
-                            handleInvalid={handleInvalid}
-                        />
-                        <Button loading={isSubmitting}>Sign In</Button>
-                    </form>
-                </div>
+                : <>
+                    <div className={styles.loginWrapper}>
+                        <form
+                            className={styles.loginForm}
+                            onSubmit={handleSubmit(submitHandler)}
+                        >
+                            <Input
+                                attributes={{
+                                    ...register("username", { required: true }),
+                                    className: styles.inputWrapper,
+                                    autoComplete: "username"
+                                }}
+                                label={<FaUser size={14} />}
+                            />
+                            <Input
+                                attributes={{
+                                    ...register("password", { required: true }),
+                                    className: styles.inputWrapper,
+                                    type: "password",
+                                    autoComplete: "current-password"
+                                }}
+                                label={<FaLock size={14} />}
+                            />
+                            <Button loading={isSubmitting}>Sign In</Button>
+                        </form>
+                    </div>
+                    <Popup {...popup} />
+                </>
             }
         </>
     );
---
+++ src/utils/errors.ts
@@ -0,0 +1,23 @@
+import { AxiosError } from "axios";
+
+type TitleAndMessage = {
+    title: string;
+    message: string;
+};
+
+export const getTitleAndMessage = (e: unknown): TitleAndMessage => {
+    console.log(e);
+    let titleAndMessage: TitleAndMessage;
+    if (e instanceof AxiosError) {
+        titleAndMessage = {
+            title: e.response?.data?.error ?? e.name,
+            message: e.response?.data?.message ?? e.message
+        };
+    } else {
+        titleAndMessage = {
+            title: "Unknown error",
+            message: "Please try again later."
+        };
+    }
+    return titleAndMessage;
+};
