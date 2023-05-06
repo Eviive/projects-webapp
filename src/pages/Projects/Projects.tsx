5106572aa0008310e99522da8af7f0c597fe0f28
Eviive <albert.vaillon21@gmail.com>
2023-05-06 02:41:04 +0200

    feat: added a functionality to create projects

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -1,28 +1,37 @@
 import { ProjectService } from "api/services";
-import { Loader } from "components/common";
+import { Button, Loader } from "components/common";
 import { ProjectCard, ProjectForm } from "components/projects";
 import { useCustomQuery } from "hooks/useCustomQuery";
 import { GridLayout } from "layouts";
 import { FC, useState } from "react";
+import { FaPlus } from "react-icons/all";
 import { Project } from "types/entities";
 
 import styles from "./projects.module.scss";
+
+type ProjectForm = {
+    project?: Project;
+    show: boolean;
+}
 
 export const Projects: FC = () => {
 
     const query = useCustomQuery(["projects"], ProjectService.findAll);
 
-    const [ editProject, setEditProject ] = useState<Project | null>(null);
+    const [ projectForm, setProjectForm ] = useState<ProjectForm>({ show: false });
 
     return (
         <>
             { query.isSuccess
 
                 ? <div className={styles.projectsWrapper}>
-                    {editProject && <ProjectForm project={editProject} handleClose={() => setEditProject(null)} />}
+                    {projectForm.show && <ProjectForm project={projectForm.project} handleClose={() => setProjectForm({ show: false })} />}
                     <GridLayout className={styles.cardsWrapper} size="350px" gap="2.5em">
-                        {[ ...query.data, ...query.data, ...query.data ].map((project, index) => <ProjectCard key={index} project={project} handleEdit={() => setEditProject(project)} />)}
+                        {[ ...query.data, ...query.data, ...query.data ].map((project, index) => <ProjectCard key={index} project={project} handleEdit={() => setProjectForm({ project, show: true })} />)}
                     </GridLayout>
+                    <Button className={styles.addButton} handleClick={() => setProjectForm({ show: true })}>
+                        <FaPlus size={22} />
+                    </Button>
                 </div>
 
                 : <Loader />
